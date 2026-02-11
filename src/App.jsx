fc618b49e00ae706c0bb85576f9c91f710dfe616
Chiranjeeb Lahiri <lchiranjeeb@outlook.com>
2026-02-12 00:54:22 +0530

    Fixed CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './App.css'
+import './App.css?inline'
 import { generateSummary, generateQuiz, generateFlashcards } from './services/gemini'
 
 function App() {
